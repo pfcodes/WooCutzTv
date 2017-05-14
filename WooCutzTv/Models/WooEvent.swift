d40d20352b7089f45a5ee943e39054fe48a2f4ad
Phil Fevry <phil.fevry@gmail.com>
2017-05-14 15:36:41 -0400

    reorganized files

---
+++ WooCutzTv/Models/WooEvent.swift
@@ -0,0 +1,9 @@
+//
+//  WooEvent.swift
+//  WooCutzTv
+//
+//  Created by Phil on 5/13/17.
+//  Copyright © 2017 phlfvry. All rights reserved.
+//
+
+import Foundation
---
+++ WooCutzTv/Models/WooService.swift
@@ -0,0 +1,9 @@
+//
+//  WooService.swift
+//  WooCutzTv
+//
+//  Created by Phil on 5/13/17.
+//  Copyright © 2017 phlfvry. All rights reserved.
+//
+
+import Foundation
